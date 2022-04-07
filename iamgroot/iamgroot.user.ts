18fd483edbd571d7eff369dc029cd8b22c57eb40
lzghzr <me@lzzr.me>
2022-04-07 21:55:50 +0800

    [iamgroot] 更新libReplaceText

--- iamgroot/iamgroot.user.ts
+++ iamgroot/iamgroot.user.ts
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        I am Groot!
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.4
+// @version     0.0.5
 // @author      lzghzr
 // @description I am Groot!
 // @match       http://*/*
 // @match       https://*/*
-// @require     https://cdn.jsdelivr.net/gh/lzghzr/TampermonkeyJS@a12e5f5baeea9c13a3dc2b6cac4b6ecef29533d5/libReplaceText/libReplaceText.js
+// @require     https://cdn.jsdelivr.net/gh/lzghzr/TampermonkeyJS@4cc2a90f51182ada0aab944ee708a893ed4ee31f/libReplaceText/libReplaceText.js
 // @license     MIT
 // @grant       none
 // @run-at      document-start
