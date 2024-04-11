3fab10b97c9f75fe631732560d0e6a0bb379d129
lzghzr <me@lzzr.me>
2024-04-11 20:03:36 +0800

    [BiLiveNoVIP] 修复挂机检测

--- BiLiveNoVIP/BiLiveNoVIP.user.ts
+++ BiLiveNoVIP/BiLiveNoVIP.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                bilibili直播净化
 // @namespace           https://github.com/lzghzr/GreasemonkeyJS
-// @version             4.2.29
+// @version             4.2.30
 // @author              lzghzr
 // @description         屏蔽聊天室礼物以及关键字, 净化聊天室环境
 // @icon                data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGNpcmNsZSBjeD0iMTYiIGN5PSIxNiIgcj0iMTUiIHN0cm9rZT0iIzAwYWVlYyIgc3Ryb2tlLXdpZHRoPSIyIiBmaWxsPSJub25lIi8+PHRleHQgZm9udC1mYW1pbHk9Ik5vdG8gU2FucyBDSksgU0MiIGZvbnQtc2l6ZT0iMjIiIHg9IjUiIHk9IjIzIiBzdHJva2U9IiMwMDAiIHN0cm9rZS13aWR0aD0iMCIgZmlsbD0iIzAwYWVlYyI+5ruaPC90ZXh0Pjwvc3ZnPg==
@@ -888,8 +888,8 @@ $<mut_n>("text",{attrs:{"font-family":"Noto Sans SC","font-size":"14",x:"5",y:"1
             else {
               console.error(GM_info.script.name, '屏蔽挂机检测失效', fnStr)
             }
+            push = push | 0x20
           }
-          push = push | 0x20
         }
         else {
           push = push | 0x20
