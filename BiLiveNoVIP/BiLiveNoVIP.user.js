b1e494ff65498e352c636b97c16694c53e5db3a8
lzghzr <me@lzzr.me>
2024-06-04 20:28:51 +0800

    [BiLiveNoVIP] 团体勋章

--- BiLiveNoVIP/BiLiveNoVIP.user.js
+++ BiLiveNoVIP/BiLiveNoVIP.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                bilibili直播净化
 // @namespace           https://github.com/lzghzr/GreasemonkeyJS
-// @version             4.2.32
+// @version             4.2.33
 // @author              lzghzr
 // @description         屏蔽聊天室礼物以及关键字, 净化聊天室环境
 // @icon                data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGNpcmNsZSBjeD0iMTYiIGN5PSIxNiIgcj0iMTUiIHN0cm9rZT0iIzAwYWVlYyIgc3Ryb2tlLXdpZHRoPSIyIiBmaWxsPSJub25lIi8+PHRleHQgZm9udC1mYW1pbHk9Ik5vdG8gU2FucyBDSksgU0MiIGZvbnQtc2l6ZT0iMjIiIHg9IjUiIHk9IjIzIiBzdHJva2U9IiMwMDAiIHN0cm9rZS13aWR0aD0iMCIgZmlsbD0iIzAwYWVlYyI+5ruaPC90ZXh0Pjwvc3ZnPg==
@@ -433,9 +433,13 @@ class NoVIP {
     }
     if (config.menu.noFansMedalIcon.enable) {
       cssText += `
-/* 聊天 */
+/* 团体勋章 */
+.chat-item .group-medal-ctnr,
+/* 团体勋章 底部提示条 */
+#brush-prompt .group-medal-ctnr,
+/* 粉丝勋章 聊天 */
 .chat-item .fans-medal-item-ctnr,
-/* 底部提示条 */
+/* 粉丝勋章 底部提示条 */
 #brush-prompt .fans-medal-item-ctnr {
   display: none !important;
 }`;
