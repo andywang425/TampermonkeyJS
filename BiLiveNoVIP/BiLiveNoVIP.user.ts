6d542cb910cfa7f59307bffa48508bc74076d0e0
lzghzr <me@lzzr.me>
2024-09-25 21:13:31 +0800

    [BiLiveNoVIP] color to opacity

--- BiLiveNoVIP/BiLiveNoVIP.user.ts
+++ BiLiveNoVIP/BiLiveNoVIP.user.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name                bilibili直播净化
 // @namespace           https://github.com/lzghzr/GreasemonkeyJS
-// @version             4.2.48
+// @version             4.2.49
 // @author              lzghzr
 // @description         增强直播屏蔽功能, 提高直播观看体验
 // @icon                data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGNpcmNsZSBjeD0iMTYiIGN5PSIxNiIgcj0iMTUiIHN0cm9rZT0iIzAwYWVlYyIgc3Ryb2tlLXdpZHRoPSIyIiBmaWxsPSJub25lIi8+PHRleHQgZm9udC1mYW1pbHk9Ik5vdG8gU2FucyBDSksgU0MiIGZvbnQtc2l6ZT0iMjIiIHg9IjUiIHk9IjIzIiBzdHJva2U9IiMwMDAiIHN0cm9rZS13aWR0aD0iMCIgZmlsbD0iIzAwYWVlYyI+5ruaPC90ZXh0Pjwvc3ZnPg==
@@ -596,8 +596,7 @@ body:not(.player-full-win):has(#anchor-guest-box-id)[style*="overflow: hidden;"]
 .bili-danmaku-x-dm.NoVIP_danmaku_hide,
 /* 官方 */
 .danmaku-item-container .mode-adv {
-  color: transparent !important;
-  text-shadow: unset !important;
+  opacity: 0 !important;
 }`
     }
     cssText += `
