dbbc640a4c723f73ef3d6e789961bd2b5f50980d
lzghzr <me@lzzr.me>
2024-10-11 22:58:39 +0800

    [BiLiveDANMUcmd] 弃用外部依赖

--- BiLiveDANMUcmd/BiLiveDANMUcmd.user.ts
+++ BiLiveDANMUcmd/BiLiveDANMUcmd.user.ts
@@ -1,18 +1,23 @@
 // ==UserScript==
 // @name        bilibili直播弹幕修复
 // @namespace   https://github.com/lzghzr/TampermonkeyJS
-// @version     0.0.2
+// @version     0.0.3
 // @author      lzghzr
 // @description 修复bilibili直播弹幕
 // @supportURL  https://github.com/lzghzr/TampermonkeyJS/issues
-// @include     /^https:\/\/live\.bilibili\.com\/(?:blanc\/)?\d/
-// @require     https://github.com/lzghzr/TampermonkeyJS/raw/a81315bc497aeeb7b5e7b9acd5aeaf41a13ef2e3/bliveproxy/bliveproxy_inline.js#sha256-voXUaa+mxDJt0KsqyZ4sGt8vS/TJndo9UZRYnh3C1h0=
+// @match       https://live.bilibili.com/*
 // @license     MIT
 // @run-at      document-start
 // ==/UserScript==
 
-const W = typeof unsafeWindow === 'undefined' ? window : unsafeWindow
-// @ts-ignore
-W.bliveproxy.addCommandHandler('ALL', command => {
-  if (command.cmd.startsWith('DANMU_MSG')) command.cmd = 'DANMU_MSG'
+Array.prototype.concat = new Proxy(Array.prototype.concat, {
+  apply: function (target, _this, args) {
+    if (args[0] && args[0] instanceof Object && args[0].cmd) {
+      const command = args[0]
+      if (command.cmd.startsWith('DANMU_MSG')) {
+        command.cmd = 'DANMU_MSG'
+      }
+    }
+    return Reflect.apply(target, _this, args)
+  }
 })
